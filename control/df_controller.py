Modified error inputs computation. Corrected one mistake in differential flatness trajectories computation.

control/df_controller.py
@@ -10,8 +10,20 @@ import numpy as np
 import model.params as params
 from math import sin, cos
 
-#import utils.dif_flat as df
+import dif_flat as dfl
 from utils.utils import RPYToRot, RPYToRot2
+
+# LQR Translational gains and reference input matrices
+Kt = np.matrix([3.162277, 2.70639])
+Nu_t = np.matrix([0.0])
+Nx_t = np.matrix([[1.0],[0.0]])
+
+
+
+# LQR Rotational gains
+Kr = 3.162277
+Nu_r = 0.0
+Nx_r = 1.0
 
 def run(quad, des_state):
 
@@ -30,7 +42,7 @@ def run(quad, des_state):
     trajectory = [pos_traj, vel_traj, acc_traj, jerk_traj, snap_traj, yaw_traj, yaw_dot_traj, yaw_ddot_traj]
 
     # compute reference states and inputs from desired trajectory
-    ref_ = df.compute_ref(trajectory)
+    ref_ = dfl.compute_ref(trajectory)
     #print(ref_)
     # extract each reference state and inputs
     x_r,y_r,z_r = ref_[0]
@@ -45,11 +57,7 @@ def run(quad, des_state):
     phi, theta, psi = quad.attitude()
     p, q, r = quad.omega()
 
-    # LQR Translational gains
-    Kt = np.matrix([3.162277, 2.70639])
 
-    # LQR Rotational gains
-    Kr = 3.162277
 
     # compute state error
     # state error = reference_state - drone_state
@@ -70,6 +78,7 @@ def run(quad, des_state):
     q_e = q_r.item(0) - q   
     r_e = r_r.item(0) - r
 
+    """
     # compute error input
     ua_e_x = -1.0*Kt*np.matrix([[x_e],[vx_e]])
     ua_e_y = -1.0*Kt*np.matrix([[y_e],[vy_e]])
@@ -85,7 +94,35 @@ def run(quad, des_state):
     uc_e_y = -1.0*Kr*theta_e
     uc_e_z = -1.0*Kr*psi_e
     uc_e = np.matrix([[uc_e_x],[uc_e_y],[uc_e_z]])
+    """
+    # In general    u = -K*x + (N_u + K*N_x)*r
+    # r = reference state
+    # x = state
+    # K = LQR gains
+    # N_u, N_x = refrence input and reference state matrices
 
+    
+    x_state = np.matrix([[x],[x_dot]])
+    ua_e_x = -Kt*x_state + (Nu_t + Kt*Nx_t)*x_r
+
+    y_state = np.matrix([[y],[y_dot]])
+    ua_e_y = -Kt*y_state + (Nu_t + Kt*Nx_t)*y_r
+
+    z_state = np.matrix([[z],[z_dot]])
+    ua_e_z = -Kt*z_state + (Nu_t + Kt*Nx_t)*z_r
+    ua_e = np.matrix([[ua_e_x.item(0)],[ua_e_y.item(0)],[ua_e_z.item(0)]])
+
+    #wx_state = np.matrix([[z],[z_dot]])
+    ub_e_x = -Kr*p + (Nu_r + Kr*Nx_r)*p_r.item(0)
+    ub_e_y = -Kr*q + (Nu_r + Kr*Nx_r)*q_r.item(0)   
+    ub_e_z = -Kr*r + (Nu_r + Kr*Nx_r)*r_r.item(0) 
+    ub_e = np.matrix([[ub_e_x],[ub_e_y],[ub_e_z]])
+
+    uc_e_x = -1.0*Kr*phi_e
+    uc_e_y = -1.0*Kr*theta_e
+    uc_e_z = -1.0*Kr*psi_e
+    uc_e = np.matrix([[uc_e_x],[uc_e_y],[uc_e_z]])    
+    
 
     # compute inputs to system
     # u_s = u_e + u_r
@@ -96,7 +133,7 @@ def run(quad, des_state):
     # Now apply the inverse function to obtain F and M  
 
     # First obtain F = Rbw*Z_b(m*ua_s +g*Z_w)
-    R = np.matrix(RPYToRot2(phi, theta, psi)) # this rotation is world to body frame
+    R = np.matrix(RPYToRot(phi, theta, psi)) # this rotation is world to body frame
     #R = np.matrix(RPYToRot2(phi_r.item(0), theta_r.item(0), psi_r.item(0))) # this rotation is world to body frame
 
     #R = R.T  # I need body to world frame
@@ -119,8 +156,8 @@ def run(quad, des_state):
     #M = ref_[8]
     #M = np.array([[M.item(0)],[M.item(1)],[M.item(2)]])
 
-    print(F.item(0))
-    print(M)
+    #print(F.item(0))
+    #print(M)
     #F = 1.0
     #M = np.array([[0.0],[0.0],[0.0]])
     return F.item(0), M

control/dif_flat.py
@@ -8,20 +8,35 @@ import numpy as np
 
 #from math import sin, cos, asin, atan2, sqrt
 
-def RotToRPY(R):
+def RotToRPY_ZYX(R):
 	"""
 		Euler angle convention is ZYX, which means first apply
 		rotaion of psi-degrees around Z axis, then rotation of
 		theta-degrees around new Y axis, and then rotation of 
 		phi-degrees around new X axis.
 
-		The rotation R received should be from body to world frame.
+		** The rotation R received should be from body to world frame. **
 	"""
 	theta = np.arcsin(-1.0*R.item(2,0))
 	phi = np.arctan2(R.item(2,1)/np.cos(theta),R.item(2,2)/np.cos(theta))
 	psi = np.arctan2(R.item(1,0)/np.cos(theta),R.item(0,0)/np.cos(theta))
 
 	return np.matrix([ [phi], [theta], [psi] ]) 
+
+def RotToRPY_ZXY(R):
+    """
+    phi, theta, psi = roll, pitch , yaw
+    The euler angle convention used is ZXY. This means: first a rotation of psi-degrees
+    around Z axis, then rotation of phi-degress around X axis, and finally rotation of
+    theta-degrees around Y axis
+
+    	** The rotation R received should be from world to body frame **
+    """
+    phi = np.arcsin(R[1,2])
+    theta = np.arctan2(-R[0,2]/np.cos(phi),R[2,2]/np.cos(phi))
+    psi = np.arctan2(-R[1,0]/np.cos(phi),R[1,1]/np.cos(phi))
+    return np.matrix([ [phi], [theta], [psi] ])
+
 
 def get_x(sigma1):
 	return sigma1
@@ -71,12 +86,12 @@ def get_yb(z_b,x_b):
 	return a/np.linalg.norm(a)
 
 def get_wx(y_b,j,u_1):
-	wx = -1.0*m*((y_b.T)*j)/u_1
-	return wx
+	w_x = -1.0*m*((y_b.T)*j)/u_1
+	return w_x
 
 def get_wy(x_b,j,u_1):
-	wy = m*((x_b.T)*j)/u_1
-	return wy
+	w_y = m*((x_b.T)*j)/u_1
+	return w_y
 
 def get_wz(psi_rate,x_c,x_b,w_y,y_c,z_b):
 	"""
@@ -85,8 +100,8 @@ def get_wz(psi_rate,x_c,x_b,w_y,y_c,z_b):
 	a = psi_rate*(x_c.T)*x_b
 	b = w_y*(y_c.T)*z_b
 	c = np.linalg.norm(np.cross(y_c,z_b,axis = 0))
-	wz = (a+b)/c
-	return wz
+	w_z = (a+b)/c
+	return w_z
 
 def get_wy_dot(x_b,s,u_1_dot,w_y,u_1,w_x,w_z):
 	"""
@@ -104,9 +119,9 @@ def get_wx_dot(y_b,s,u_1_dot,w_x,u_1,w_y,w_z):
 	"""
 		Will use wx_dot = (a + b + c)/d
 	"""
-	a = (y_b.T)*s
-	b = 2.0*u_1_dot*w_x/m
-	c = -1.0*u_1*w_y*w_z/m
+	a = -1.0*(y_b.T)*s
+	b = -2.0*u_1_dot*w_x/m
+	c = u_1*w_y*w_z/m
 	d = u_1/m
 	w_x_dot = (a+b+c)/d
 	return w_x_dot
@@ -221,10 +236,17 @@ def compute_ref(trajectory):
 	# This matrix represents the orientation of the quadrotor
 	R_ = np.concatenate((x_b, y_b, z_b), axis = 1)
 
-	# Get rall pitch yaw angles assuming ZYX Euler angle convention
+	# Get roll pitch yaw angles assuming ZXY Euler angle convention
 	# This means: first rotate psi degrees around Z axis,
 	# then theta degrees around Y axis, and lastly phi degrees around X axis
-	or_ = RotToRPY(R_)
+	
+	#or_ = RotToRPY_ZYX(R_)  # assuming ZYX Eugler angle convention, so sent matrix should be 
+							  # body - world frame
+
+	or_ = RotToRPY_ZXY(R_.T)  # assuming ZXY Eugler angle convention, so sent matrix should be 
+							  # world - body frame
+
+
 
 	# compute u_a input for system reference
 	# can be computed as follows or simply the received acc_traj
@@ -246,7 +268,7 @@ def compute_ref(trajectory):
 
 	# we send the received pos_traj, and vel_traj vectors as the reference pos and vel vectors
 	# because that is the result from the differential flatness output selection
-	return [pos_traj.T, vel_traj.T, or_, w_, u_a, u_b, u_c, u_1, u_x]
+	return [pos_traj.T, vel_traj.T, or_, w_, acc_traj.T, w_dot_, u_c, u_1, u_x]
 
 
 

trajGen3D.py
@@ -10,7 +10,7 @@ from numpy.linalg import inv
 from collections import namedtuple
 from numpy import linalg as LA
 
-DesiredState = namedtuple('DesiredState', 'pos vel acc yaw yawdot')
+DesiredState = namedtuple('DesiredState', 'pos vel acc jerk snap yaw yawdot yawddot')
 yaw = 0.0
 current_heading = np.array([1.0,0.0]) #heading vector should always be unitary vector. [0,0] vector has no heading!
 
@@ -33,7 +33,7 @@ def get_poly_waypoints(t,n):
             z = k2*t
         Output waypoints shape is [n, 3]
     """
-    waypoints_t = np.linspace(0, t + 4*np.pi, n)
+    waypoints_t = np.linspace(0, t, n)
     k1 = 0.0
     k2 = 0.0
     #x = (k1*waypoints_t)**2
@@ -90,6 +90,9 @@ def generate_trajectory(t, v, waypoints, coeff_x, coeff_y, coeff_z):
     pos = np.zeros(3)
     acc = np.zeros(3)
     vel = np.zeros(3)
+    jerk = np.zeros(3)
+    snap = np.zeros(3)
+    yawddot = 0.0
 
     # distance vector array, represents each segment's distance
     distance = waypoints[0:-1] - waypoints[1:]
@@ -113,7 +116,7 @@ def generate_trajectory(t, v, waypoints, coeff_x, coeff_y, coeff_z):
             #  if velocity vector is of zero magnitude there should be no change in heading!
             pass
         else:
-            current_heading = v_proj/LA.norm(v_proj)
+            current_heading = v_proj/LA.norm(v_proj) * (1.0 / T[0])
             
 
     # stay hover at the last waypoint position
@@ -135,6 +138,14 @@ def generate_trajectory(t, v, waypoints, coeff_x, coeff_y, coeff_z):
         t2 = get_poly_cc(8, 2, scale)
         # chain rule applied
         acc = np.array([coeff_x[start:end].dot(t2), coeff_y[start:end].dot(t2), coeff_z[start:end].dot(t2)]) * (1.0 / T[t_index]**2)
+
+        t3 = get_poly_cc(8, 3, scale)
+        # apply chain rule
+        jerk = np.array([coeff_x[start:end].dot(t3), coeff_y[start:end].dot(t3), coeff_z[start:end].dot(t3)]) * (1.0 / T[t_index]**3)
+
+        t4 = get_poly_cc(8, 4, scale)
+        # apply chain rule
+        snap = np.array([coeff_x[start:end].dot(t4), coeff_y[start:end].dot(t4), coeff_z[start:end].dot(t4)]) * (1.0 / T[t_index]**4)
 
         # calculate desired yaw and yaw rate
 
@@ -179,12 +190,11 @@ def generate_trajectory(t, v, waypoints, coeff_x, coeff_y, coeff_z):
         yawdot = delta_psi / 0.005 # dt is control period
         max_yawdot = 5.0 #rad/s
         if(abs(yawdot) > max_yawdot):
-            yawdot = (yawdot/abs(yawdot))*5.0 # make it 5rad/s with appropriate direction
+            yawdot = (yawdot/abs(yawdot))*max_yawdot # make it 5rad/s with appropriate direction
         
         yaw = 0
         yawdot = 0
-        print (" yaw: {}  yawdot: {}".format(yaw,yawdot))
-    return DesiredState(pos, vel, acc, yaw, yawdot)
+    return DesiredState(pos, vel, acc, jerk, snap, yaw, yawdot, yawddot)
 
 def get_poly_cc(n, k, t):
     """ This is a helper function to get the coeffitient of coefficient for n-th